b359682cf7e2e7b0331e10c98ab0fbc0fbd74e26
jayalodha <jayalodha@gmail.com>
2020-01-22 05:48:49 +0000

    first commit

---
+++ MohitSolution/MohitSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MohitSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
